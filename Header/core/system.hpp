529c0ce2ad7da5a5e01756acd549098006c3bd2b
gravitywolfLaCanneAPeche <stanislas.morin6@gmail.com>
2025-03-29 15:55:37 +0100

    group / view system specialization

--- Header/core/system.hpp
+++ Header/core/system.hpp
@@ -22,10 +22,36 @@ namespace dg
     bool active = true;
   };
 
+  template <class ...Components>
+  struct GroupFactory
+  {
+    static auto create(entt::registry* pRegistry)
+    {
+      return pRegistry->group<Components...>();
+    }
+  };
+
+  template <class Component>
+  struct ViewFactory
+  {
+    static auto create(entt::registry* pRegistry)
+    {
+      return pRegistry->view<Component>();
+    }
+  };
+
   template <typename... Components>
   class System : public ISystem
   {
     using FirstComponent = std::tuple_element_t<0, std::tuple<Components...>>;
+
+    // Specialization because views are better for one component and groups for
+    // multiple components
+    using Factory = typename std::conditional<
+      sizeof...(Components) == 1,
+      ViewFactory<FirstComponent>,
+      GroupFactory<Components...>
+        >::type;
 
     public:
 
@@ -34,7 +60,7 @@ namespace dg
         assert(pRegistry != nullptr && "pRegistry was not assigned");
         assert(pScene != nullptr && "pScene was not assigned");
 
-        auto group = pRegistry->view<Components...>();
+        auto group = Factory::create(pRegistry);
         for (auto entity : group)
         {
           std::apply([entity, this](auto &&... args) {
